a0274f33ac192a6f96b2a228c9412cbc9b86bcdd
Naod Yemane <naodyemane96@gmail.com>
2025-09-01 16:54:22 +0300

    just little changes

--- config/db.js
+++ config/db.js
@@ -3,5 +3,5 @@ exports.connectMonggose =()=>{
     mongoose.set("strictQuery", false);
     mongoose.connect(process.env.DATABASE_URL,)
     .then((e)=>console.log("Connected to Mongodb =>> hospital_managementDB Project"))
-    .catch((e)=>console.log("Not Connect Mongodb"))
+    .catch((e)=>console.log("Not Connected to Mongodb"))
 }
